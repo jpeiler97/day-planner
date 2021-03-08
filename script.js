1d757f2fdd06e85fdc3858bdeea1db67077c21e8
JP Eiler <jp.eiler97@gmail.com>
2021-03-08 10:57:30 -0600

    added timeblock color changes based on time

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-var timeText = document.querySelector('#currentDay');
-
+var timeText = $('#currentDay');
+var hourBlock = $('.hour');
 timeText.textContent = moment().format('MMMM Do, YYYY');
+var timeblockCount = $('.blockArea').children().length;
+
+for (i = 0; i < timeblockCount; i++) {
+	if ($('.blockArea').children().eq(i).children('.hour').text().substring(0, 2) === moment().format('HH')) {
+		$('.blockArea')
+			.children()
+			.eq(i)
+			.children('.hour')
+			.siblings('.past')
+			.addClass('present')
+			.toggleClass('.present');
+	} else if ($('.blockArea').children().eq(i).children('.hour').text().substring(0, 2) > moment().format('HH')) {
+		$('.blockArea').children().eq(i).children('.hour').siblings('.past').addClass('future').toggleClass('.future');
+	}
+}
