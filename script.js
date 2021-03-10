f16ec605a93dce73521a05201fb8e46d54cc50e0
JP Eiler <jp.eiler97@gmail.com>
2021-03-10 10:06:27 -0600

    added textareas to timeblocks

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var timeText = $('#currentDay');
-var hourBlock = $('.hour');
 $('#currentDay').text(moment().format('MMMM Do, YYYY'));
 var timeblockCount = $('.blockArea').children().length;
 var currentHour = moment();
 
+//Iterates over number of time block elements
 for (i = 0; i < timeblockCount; i++) {
+	//Stores time string for specific time block into format that can be compared using moment.js (HH A)
 	var blockHour = moment($('.blockArea').children().eq(i).children('.hour').text(), 'HH A');
-	console.log(' ');
-	console.log('Block hour:' + blockHour);
-	console.log('Current hour:' + currentHour);
+
+	//Changes class of time block based on if time block's hour matches, is before, or is after currentHour
 	if (blockHour.isSame(currentHour, 'hour') === true) {
+		//Sets class to present if blockHour is equal to currentHour
 		$('.blockArea')
 			.children()
 			.eq(i)
@@ -18,8 +18,10 @@ for (i = 0; i < timeblockCount; i++) {
 			.addClass('present')
 			.toggleClass('.present');
 	} else if (blockHour.isBefore(currentHour, 'hour') === true) {
+		//Sets class to past if blockHour is before currentHour
 		$('.blockArea').children().eq(i).children('.hour').siblings('.past').addClass('past').toggleClass('.past');
 	} else if (blockHour.isAfter(currentHour, 'hour') === true) {
+		//Sets class to future if blockHour is before currentHour
 		$('.blockArea').children().eq(i).children('.hour').siblings('.past').addClass('future').toggleClass('.future');
 	}
 }
